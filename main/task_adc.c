7030f32e228a5a70fcfaf19cb5cf9e46ffb2b495
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2021-05-27 22:23:54 -0700

    task_adc.c: update VRAIL/counts_rail for new resistor values

--- main/task_adc.c
+++ main/task_adc.c
@@ -24,7 +24,7 @@ void task_adc(void *arg) {
         VLIPO = counts_lipo * 0.0010586798822092942;    // 26.8 * 1.1 / 6.8 / 4095
 
         counts_rail = adc1_get_raw(ADC_CHANNEL_5);
-        VRAIL = counts_rail * 0.006007326007326007;     // 49.2 * 1.1 / 2.2 / 4095
+        VRAIL = counts_rail * 0.015490435490435493;     // 51900. * 1.1 / 900. / 4095
 
         printf("\ncounts_lipo = %d\n", counts_lipo);
         printf("counts_rail = %d\n", counts_rail);
