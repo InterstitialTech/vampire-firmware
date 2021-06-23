5466bb9e8aefb5885fe95ce522090784f57eb2ec
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2021-06-22 22:09:09 -0700

    task_adc.c: implement multisample averaging

--- main/task_adc.c
+++ main/task_adc.c
@@ -6,9 +6,23 @@
 float VLIPO = 0.;
 float VRAIL = 0.;
 
+#define NUM_MEASUREMENTS 64
+
+float get_avg_adc_count(adc1_channel_t chan) {
+
+    float accumulator = 0.;
+
+    for (int i=0; i<NUM_MEASUREMENTS; i++) {
+        accumulator += adc1_get_raw(chan);
+    }
+
+    return accumulator / NUM_MEASUREMENTS;
+
+}
+
 void task_adc(void *arg) {
 
-    uint16_t counts_lipo, counts_rail;
+    float counts_lipo, counts_rail;
 
     adc1_config_width(ADC_WIDTH_BIT_12);
 
@@ -20,14 +34,14 @@ void task_adc(void *arg) {
 
     while (1) {
 
-        counts_lipo = adc1_get_raw(ADC_CHANNEL_0);
+        counts_lipo = get_avg_adc_count(ADC_CHANNEL_0);
         VLIPO = counts_lipo * 0.0010586798822092942;    // 26.8 * 1.1 / 6.8 / 4095
 
-        counts_rail = adc1_get_raw(ADC_CHANNEL_5);
+        counts_rail = get_avg_adc_count(ADC_CHANNEL_5);
         VRAIL = counts_rail * 0.015490435490435493;     // 51900. * 1.1 / 900. / 4095
 
-        printf("\ncounts_lipo = %d\n", counts_lipo);
-        printf("counts_rail = %d\n", counts_rail);
+        printf("\ncounts_lipo = %.3f\n", counts_lipo);
+        printf("counts_rail = %.3f\n", counts_rail);
         printf("VLIPO = %.3f\n", VLIPO);
         printf("VRAIL = %.3f\n\n", VRAIL);
 
